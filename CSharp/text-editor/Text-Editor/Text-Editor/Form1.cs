2185e64917922acb4a037756e67f8ecb9ca52293
Rechard Nabo <32723733+RechardNabo@users.noreply.github.com>
2020-03-11 22:04:32 +0200

    me

---
+++ CSharp/text-editor/Text-Editor/Text-Editor/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string data;
+            
+        }
+
+        public void File()
+        {
+           // myfile = open
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private bool mouseDown;
+        private Point lastLocation;
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown =  true;
+            lastLocation = e.Location;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown) {
+                this.Location = new Point((Location.X - lastLocation.X)+e.X , (this.Location.Y-lastLocation.Y) + e.Y);
+                this.Update();
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
